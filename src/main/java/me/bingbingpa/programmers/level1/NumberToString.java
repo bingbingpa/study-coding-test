6033eef69f33a6ea3e5908719b00b2d48a133491
shpark <bingbingpa@gmail.com>
2021-05-09 22:03:36 +0900

    입력 문자열을 숫자로 변환하여 리턴하라

---
+++ src/main/java/me/bingbingpa/programmers/level1/NumberToString.java
@@ -0,0 +1,45 @@
+package me.bingbingpa.programmers.level1;
+
+import java.util.Map;
+
+public class NumberToString {
+    public static void main(String[] args) {
+        NumberToString numberToString = new NumberToString();
+        System.out.println("result ============= " + numberToString.solution("one4seveneight"));
+    }
+
+    /**
+     * TODO 입력 문자열을 숫자로 변환하여 리턴하라.
+     */
+    public int solution(String str) {
+        StringBuilder sb = new StringBuilder();
+        Map<String, Integer> map = Map.of(
+                "zero", 0,
+                "one", 1,
+                "two", 2,
+                "three", 3,
+                "four", 4,
+                "five", 5,
+                "six", 6,
+                "seven", 7,
+                "eight", 8,
+                "nine", 9
+        );
+
+        StringBuilder temp = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+            int target = ch - '0';
+            if (target < 10) {
+                sb.append(ch);
+            } else {
+                temp.append(ch);
+            }
+            if (map.containsKey(temp.toString())) {
+                sb.append(map.get(temp.toString()));
+                temp.setLength(0);
+            }
+        }
+
+        return Integer.parseInt(sb.toString());
+    }
+}
