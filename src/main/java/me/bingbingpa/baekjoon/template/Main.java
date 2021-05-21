dbef93c90940e2c091c40ef560f91d5cc33b2e0a
shpark <bingbingpa@gmail.com>
2021-05-21 11:26:10 +0900

    백준 온라인 Main 클래스 템플릿

---
+++ src/main/java/me/bingbingpa/baekjoon/template/Main.java
@@ -0,0 +1,16 @@
+package me.bingbingpa.baekjoon.template;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        scanner.close();
+        System.out.println(solution(number));
+    }
+
+    public static long solution(int number) {
+        return 0;
+    }
+}
