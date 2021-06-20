7242dfe0561a395b88563e4d1a0a7c56e15b741b
shpark <bingbingpa@gmail.com>
2021-06-21 08:39:31 +0900

    자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요.(에라토스네테스 체)

---
+++ src/main/java/me/bingbingpa/inflearn/taewon/_02_array/NumberOfDecimals.java
@@ -0,0 +1,37 @@
+package me.bingbingpa.inflearn.taewon._02_array;
+
+import java.util.Scanner;
+
+public class NumberOfDecimals {
+
+//    public static void main(String[] args) {
+//        NumberOfDecimals T = new NumberOfDecimals();
+//        System.out.println(T.solution(20));
+//    }
+
+    public static void main(String[] args) {
+        NumberOfDecimals T = new NumberOfDecimals();
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(T.solution(num));
+    }
+
+    /**
+     * TODO 자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요.(에라토스네테스 체)
+     * 첫 줄에 자연수의 개수 N(2<=N<=200,000)이 주어집니다.
+     */
+    public int solution(int num) {
+        int answer = 0;
+        int[] ch = new int[num + 1];
+        for (int i = 2; i <= num; i++) {
+            if (ch[i] == 0) {
+                answer++;
+                for (int j = i; j <= num; j = j + i) {
+                    ch[j] = 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
