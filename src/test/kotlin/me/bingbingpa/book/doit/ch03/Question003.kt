4e86952d4cc2157a415b9ed0fcd46f8468c5c1e0
shpark <bingbingpa@gmail.com>
2023-01-14 21:11:11 +0900

    Do it 알고리즘 코딩 테스트 자바편: import 수정

--- src/test/kotlin/me/bingbingpa/book/doit/ch03/Question003.kt
+++ src/test/kotlin/me/bingbingpa/book/doit/ch03/Question003.kt
@@ -1,6 +1,7 @@
 package me.bingbingpa.book.doit.ch03
 
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -14,7 +15,7 @@ class Question003 {
     @ParameterizedTest
     fun `구간 합 구하기`(numbers: IntArray, expected: Int) {
         val target = intArrayOf(5, 4, 3, 2, 1)
-        Assertions.assertThat(solution(target, numbers)).isEqualTo(expected)
+        assertThat(solution(target, numbers)).isEqualTo(expected)
     }
 
     private fun getParameters(): Stream<Arguments> {
